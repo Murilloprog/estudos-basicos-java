1841a1cbb1b579191f1dbed1ee35669c99f30114
Murillo LP <murillo_prog@outlook.com>
2023-07-20 16:03:39 +0200

    excecoes

---
+++ src/terceirasemana/Excecoes.java
@@ -0,0 +1,44 @@
+package terceirasemana;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Excecoes {
+    public static void main(String[] args) {
+        try{
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();   
+
+        }
+        catch (InputMismatchException z){
+            System.out.println("Os campos idade e altura precisam ser numéricos");
+        }
+        
+    }
+    
+}
+
+/*A linguagem Java, dispõe de uma vasta lista de classes que representam exceções,
+ abaixo iremos apresentar as mais comuns:
+java.lang.NullPointerException - Quando tentamos obter alguma informação de uma variável nula.
+java.lang.ArithmeticException - Quando tentamos dividir um valor por zero.
+java.sql.SQLException - Quando existe algum erro, relacionado a interação com banco de dados.
+java.io.FileNotFoundException - Quando tentamos ler ou escrever, em um arquivo que não existe.*/
