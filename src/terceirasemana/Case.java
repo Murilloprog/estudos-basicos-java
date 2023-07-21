5ee2805571a61a958d3b67a286809bdafe26802b
Murillo LP <murillo_prog@outlook.com>
2023-07-21 16:24:55 +0200

    while

---
+++ src/terceirasemana/Case.java
@@ -0,0 +1,53 @@
+package terceirasemana;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+//Processo seletivo, faremos um controle de candidatos. 
+
+public class Case {
+    public static void main(String[] args) {
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+        // Array com a lista de candidatos
+
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor " + salarioPretendido);
+            if (salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato +  " foi selecionado para vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+
+        }
+}
+
+    static double valorPretendido() {
+     return ThreadLocalRandom.current().nextDouble(1800, 2200);
+}
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        }
+
+        else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o candidato com contra proposta");
+        }
+        else {
+            System.out.println("Aguardando resultado dos demais candidatos");
+        }
+
+    }
+}
